fc8968aab42f821e3c770ad80a8bc57e42ec426c
shitalm277 <shitalmehta277@gmail.com>
2021-10-17 15:20:47 +0100

    practice

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Shital");
+        System.out.println("28");
+
+
+
+    }
+
+}
---
+++ src/PersonalDetai8ls.java
@@ -0,0 +1,5 @@
+public class PersonalDetai8ls {
+    public static void main(String[] args) {
+        System.out.println("address");
+    }
+}
